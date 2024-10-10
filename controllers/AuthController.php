9c8ea4ccfa5a08b45b4032d99271e4c1b4603d80
nonamee12 <kurogane.ikki1223@gmail.com>
2024-10-10 12:50:22 +0700

    add login

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -6,8 +6,45 @@ class AuthController
 {
     public  function index()
     {
-        var_dump("dsa");
-
         return view("auth/login");
+    }
+
+    public function authentic()
+    {
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $hasError = false;
+        if (empty($username)) {
+            $_SESSION["usenameErr"] = 'Username tidak boleh kosong';
+            $hasError = true;
+        }
+
+        if (empty($password)) {
+            $_SESSION["passwordErr"] = 'Kata sandi tidak boleh kosong';
+            $hasError = true;
+        } else if (strlen($password) < 8) {
+            $_SESSION["passwordErr"] = 'Kata sandi minimal 8 karakter';
+            $hasError = true;
+        }
+        //Cek inputan ke database
+        $user = "SELECT * FROM user where user = $username";
+        if ($user) { //kondisi jika Username terdaftar
+            if (md5($password) === $user) {
+                $_SESSION["user"] = $user;
+                header("Location:/login");
+                //Mengirim user ke Dashboard
+            } else { //kondisi jika password salah
+                $_SESSION["usernameErr"] = 'Password salah';
+            }
+        } else { //kondisi jika E-mail tidak terdaftar
+            $_SESSION["usernameErr"] = 'Username tidak terdaftar';
+        }
+
+        if (!$hasError) {
+            // Login logic here      
+        } else {
+            header("Location:/login");
+        }
     }
 }
--- route/route.php
+++ route/route.php
@@ -3,9 +3,11 @@
 use controllers\AuthController;
 use services\Route;
 
-Route::addRoute("GET", "/login", function () {
-    return view("auth/login");
-});
+// Route::addRoute("GET", "/login", function () {
+//     return view("auth/login");
+// });
+Route::addRoute("GET", "/login", [AuthController::class, "index"]);
+Route::addRoute("POST", "/login", [AuthController::class, "authentic"]);
 
 
 Route::run();
