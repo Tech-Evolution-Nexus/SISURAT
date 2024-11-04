4d5226661f56dcf4935d29ff871f0bf0612624f3
Muhammad kholit <mnorkholit7@gmail.com>
2024-11-04 21:05:33 +0700

    update

--- route/web.php
+++ route/web.php
@@ -27,41 +27,35 @@ Router::addRoute("GET", "/admin/surat-selesai/{id}", [SuratMasukSelesaiControlle
 Router::addRoute("GET", "/admin/kartu-keluarga", [KartuKeluargaController::class, "index"]);
 Router::addRoute("GET", "/admin/kartu-keluarga/create", [KartuKeluargaController::class, "create"]);
 Router::addRoute("POST", "/admin/kartu-keluarga", [KartuKeluargaController::class, "store"]);
-Router::addRoute("GET", "/admin/kartu-keluarga/:id/edit", [KartuKeluargaController::class, "edit"]);
-Router::addRoute("POST", "/admin/kartu-keluarga/:id", [KartuKeluargaController::class, "update"]);
-Router::addRoute("GET", "/admin/kartu-keluarga/:id/delete", [KartuKeluargaController::class, "delete"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{id}/edit", [KartuKeluargaController::class, "edit"]);
+Router::addRoute("POST", "/admin/kartu-keluarga/{id}", [KartuKeluargaController::class, "update"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{id}/delete", [KartuKeluargaController::class, "delete"]);
 
 
 //anggota kk
-Router::addRoute("GET", "/admin/kartu-keluarga/:nokk/anggota-keluarga", [AnggotaKeluargaController::class, "index"]);
-Router::addRoute("GET", "/admin/kartu-keluarga/:nokk/anggota-keluarga/create", [AnggotaKeluargaController::class, "create"]);
-Router::addRoute("POST", "/admin/kartu-keluarga/:nokk/anggota-keluarga", [AnggotaKeluargaController::class, "store"]);
-Router::addRoute("GET", "/admin/kartu-keluarga/:nokk/anggota-keluarga/:nik/edit", [AnggotaKeluargaController::class, "edit"]);
-Router::addRoute("POST", "/admin/kartu-keluarga/:nokk/anggota-keluarga/:nik", [AnggotaKeluargaController::class, "update"]);
-Router::addRoute("GET", "/admin/kartu-keluarga/:nokk/anggota-keluarga/:nik/delete", [AnggotaKeluargaController::class, "delete"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{nokk}/anggota-keluarga", [AnggotaKeluargaController::class, "index"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{nokk}/anggota-keluarga/create", [AnggotaKeluargaController::class, "create"]);
+Router::addRoute("POST", "/admin/kartu-keluarga/{nokk}/anggota-keluarga", [AnggotaKeluargaController::class, "store"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{nokk}/anggota-keluarga/{nik}/edit", [AnggotaKeluargaController::class, "edit"]);
+Router::addRoute("POST", "/admin/kartu-keluarga/{nokk}/anggota-keluarga/{nik}", [AnggotaKeluargaController::class, "update"]);
+Router::addRoute("GET", "/admin/kartu-keluarga/{nokk}/anggota-keluarga/{nik}/delete", [AnggotaKeluargaController::class, "delete"]);
 
 // RT DAN RW 
 // RW
 Router::addRoute("GET", "/admin/master-rw", [RT_RWController::class, "indexRW"]);
-Router::addRoute("GET", "/admin/master-rw/ajax-rw/:nik", [RT_RWController::class, "ajaxRW"]);
-Router::addRoute("GET", "/admin/master-rw/ajax-masyarakat/:nik", [RT_RWController::class, "ajaxMasyarakat"]);
+Router::addRoute("GET", "/admin/master-rw/ajax-rw/{nik}", [RT_RWController::class, "ajaxRW"]);
+Router::addRoute("GET", "/admin/master-rw/ajax-masyarakat/{nik}", [RT_RWController::class, "ajaxMasyarakat"]);
 Router::addRoute("GET", "/admin/master-rw/create", [RT_RWController::class, "createRW"]);
 Router::addRoute("POST", "/admin/master-rw", [RT_RWController::class, "storeRW"]);
-Router::addRoute("POST", "/admin/master-rw/:nik", [RT_RWController::class, "updateRW"]);
+Router::addRoute("POST", "/admin/master-rw/{nik}", [RT_RWController::class, "updateRW"]);
 
 // RT
-Router::addRoute("GET", "/admin/master-rw/:rw", [RT_RWController::class, "indexRT"]);
-Router::addRoute("GET", "/admin/master-rw/:rw/ajax-rt/:nik", [RT_RWController::class, "ajaxRT"]);
-Router::addRoute("GET", "/admin/master-rw/:rw/ajax-masyarakat/:nik", [RT_RWController::class, "ajaxMasyarakat"]);
-Router::addRoute("GET", "/admin/master-rw/:rw/create", [RT_RWController::class, "createRT"]);
-Router::addRoute("POST", "/admin/master-rw/:rw", [RT_RWController::class, "storeRT"]);
-Router::addRoute("POST", "/admin/master-rw/:rw/:nik", [RT_RWController::class, "updateRT"]);
+Router::addRoute("GET", "/admin/master-rw/{rw}/master-rt", [RT_RWController::class, "indexRT"]);
+Router::addRoute("GET", "/admin/master-rw/{rw}/master-rt/ajax-rt/{nik}", [RT_RWController::class, "ajaxRT"]);
+Router::addRoute("GET", "/admin/master-rw/{rw}/master-rt/create", [RT_RWController::class, "createRT"]);
+Router::addRoute("POST", "/admin/master-rw/{rw}/master-rt", [RT_RWController::class, "storeRT"]);
+Router::addRoute("POST", "/admin/master-rw/{rw}/master-rt/{nik}", [RT_RWController::class, "updateRT"]);
 
-// RW
-Router::addRoute("GET", "/admin/master-rt/:rw-id", [RT_RWController::class, "indexRT"]);
-Router::addRoute("GET", "/admin/master-rt/:rw-id/create", [RT_RWController::class, "createRT"]);
-Router::addRoute("POST", "/admin/master-rt/:rw-id", [RT_RWController::class, "storeRT"]);
-Router::addRoute("POST", "/admin/master-rt/:rw-id/:id", [RT_RWController::class, "editRT"]);
 
 // login 
 Router::addRoute("GET", "/login", [AuthController::class, "index"]);
@@ -74,4 +68,3 @@ Router::addRoute("POST", "/sendemail", [AuthController::class, 'sendemail']);
 //ganti password
 Router::addRoute("GET", "/ganti-password", [AuthController::class, "gantiPassword"]);
 Router::addRoute("POST", "/ganti-password", [AuthController::class, "gantiPasswordStore"]);
-
