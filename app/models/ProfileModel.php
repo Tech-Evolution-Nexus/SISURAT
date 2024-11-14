3a4c4543dc7c358b4ad018d523f0dc37be9a72a7
nonamee12 <kurogane.ikki1223@gmail.com>
2024-11-14 12:07:00 +0700

    nambah model dan fungsi upload pp admin
    permark dikit dibagian profile, tentang aplikasi dan nambahin profilemodel(belum selesai)

---
+++ app/models/ProfileModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\models;
+
+use app\abstract\Model;
+
+class UserModel extends Model
+{
+    protected $table = "users";  // Nama tabel
+    protected $primaryKey = "id"; // Nama primary key
+    protected $fillable = ["email", "password", "no_hp"]; // Kolom yang bisa diisi
+
+    public function user()
+    {
+        $idUser = session()->get("user_id");
+        $user = $this->where("id", "=", $idUser)->join("masyarakat", "users.nik", "masyarakat.nik")->first();
+        return $user ?? null;
+    }
+    public function check()
+    {
+        $idUser = session()->get("user_id");
+        $user = $this
+            ->select("users.id,nama_lengkap,role,email")
+            ->where("id", "=", $idUser)
+            ->join("masyarakat", "users.nik", "masyarakat.nik")->first();
+        return !!$user;
+    }
+}
